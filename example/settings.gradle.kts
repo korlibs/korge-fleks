a37c78f57469de9aaf69537325e41731cb94f8bf
soywiz <soywiz@gmail.com>
2023-05-05 00:39:23 +0200

    kproject 0.2.1

--- example/settings.gradle.kts
+++ example/settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement { repositories {  mavenLocal(); mavenCentral(); google(); gradlePluginPortal()  }  }
 
 plugins {
-    id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
-    //id("com.soywiz.kproject.settings") version "0.2.1"
+    //id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
+    id("com.soywiz.kproject.settings") version "0.2.1"
 }
 
 kproject("./deps")
