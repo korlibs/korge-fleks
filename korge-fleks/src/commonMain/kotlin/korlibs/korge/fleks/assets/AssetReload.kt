7884db81a1c8ad20bcba760911234b1e5d820e3e
Marko Koschak <marko.koschak@tisno.de>
2024-01-27 13:39:02 +0100

    Refactor AssetReload
    - Introduce DSL for configuring Resource Directory watcher and Asset
      reloading callbacks

--- korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/assets/AssetReload.kt
+++ korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/assets/AssetReload.kt
@@ -1,6 +1,5 @@
 package korlibs.korge.fleks.assets
 
-import com.github.quillraven.fleks.World
 import korlibs.datastructure.setExtra
 import korlibs.image.font.*
 import korlibs.image.format.ASE
@@ -11,43 +10,99 @@ import korlibs.io.file.Vfs
 import korlibs.io.file.VfsFile
 import korlibs.io.file.fullName
 import korlibs.io.file.std.resourcesVfs
-import korlibs.io.lang.Closeable
-import korlibs.korge.fleks.components.*
-import korlibs.korge.fleks.entity.config.ParallaxBackground.configureParallaxLayers
-import korlibs.korge.fleks.familyHooks.onDrawableFamilyAdded
-import korlibs.korge.fleks.familyHooks.onDrawableFamilyRemoved
-import korlibs.korge.fleks.familyHooks.onSpecificLayerFamilyAdded
-import korlibs.korge.fleks.familyHooks.onSpecificLayerFamilyRemoved
 import korlibs.korge.parallax.readParallaxDataContainer
-import kotlinx.coroutines.delay
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.*
+import kotlin.coroutines.*
+import kotlin.native.concurrent.*
 
-class AssetReload(private val assetStore: AssetStore = AssetStore) {
+
+class AssetsWatcherConfiguration {
+    internal var type: AssetType = AssetType.None
+    internal var toBeStarted: Boolean = false
+    internal var enabled: Boolean = false
+    internal var imageChangedCallback: MutableList<() -> Unit> = mutableListOf()
+    internal var fontChangedCallback: MutableList<() -> Unit> = mutableListOf()
+    internal var backgroundCallback: MutableList<() -> Unit> = mutableListOf()
+
+    fun onImageChanged(callback: () -> Unit) { imageChangedCallback += callback }
+
+    fun onFontChanged(callback: () -> Unit) { fontChangedCallback += callback }
+
+    fun onBackgroundChanged(callback: () -> Unit) { backgroundCallback += callback }
+}
+
+class ResourceDirWatcherConfiguration {
+    internal var commonAssetsWatcher = AssetsWatcherConfiguration()
+    internal var worldAssetsWatcher = AssetsWatcherConfiguration()
+    internal var levelAssetsWatcher = AssetsWatcherConfiguration()
+    internal var specialAssetsWatcher = AssetsWatcherConfiguration()
 
     private var reloading: Boolean = false  // Used for debouncing reload of config (in case the modification message comes twice from the system)
-    private lateinit var commonResourcesWatcher: Closeable
-    private lateinit var currentWorldResourcesWatcher: Closeable
-    private lateinit var currentLevelResourcesWatcher: Closeable
 
-    private var callback: () -> Unit = {}
+    fun addAssetWatcherAll(callback: AssetsWatcherConfiguration.() -> Unit) {
+        commonAssetsWatcher.enabled = true
+        worldAssetsWatcher.enabled = true
+        levelAssetsWatcher.enabled = true
+        specialAssetsWatcher.enabled = true
+        commonAssetsWatcher.apply(callback)
+        worldAssetsWatcher.apply(callback)
+        levelAssetsWatcher.apply(callback)
+        specialAssetsWatcher.apply(callback)
+    }
 
-    suspend fun watchForChanges(world: World, assetReloadContext: CoroutineContext, block: () -> Unit = {}) {
-        callback = block
-        resourcesVfs["."].listRecursiveSimple().forEach { file ->
-            if (file.stat().isDirectory) {
-                println("Add watcher for '${file.path}'")
-                file.watch {
-                    if (it.kind == Vfs.FileEvent.Kind.MODIFIED) {
-                        checkAssetFolders(world, it.file, AssetType.Common, assetStore.commonAssetConfig, assetReloadContext)
-                        checkAssetFolders(world, it.file, AssetType.World, assetStore.currentWorldAssetConfig, assetReloadContext)
-                        checkAssetFolders(world, it.file, AssetType.Level, assetStore.currentLevelAssetConfig, assetReloadContext)
-                    }
-                }
+    fun addAssetWatcher(type: AssetType, callback: AssetsWatcherConfiguration.() -> Unit) {
+        when(type) {
+            AssetType.None -> {}  // Do nothing
+            AssetType.Common -> {
+                commonAssetsWatcher.toBeStarted = true
+                commonAssetsWatcher.apply(callback)
+            }
+            AssetType.World -> {
+                worldAssetsWatcher.toBeStarted = true
+                worldAssetsWatcher.apply(callback)
+            }
+            AssetType.Level -> {
+                levelAssetsWatcher.toBeStarted = true
+                levelAssetsWatcher.apply(callback)
+            }
+            AssetType.Special -> {
+                specialAssetsWatcher.toBeStarted = true
+                specialAssetsWatcher.apply(callback)
             }
         }
     }
 
-    private suspend fun checkAssetFolders(world: World, file: VfsFile, type: AssetType, assetConfig: AssetModel, assetReloadContext: CoroutineContext) = with (world) {
+    /**
+     * Configures the resource directory watcher for each enabled asset type.
+     */
+    suspend fun configure() : ResourceDirWatcherConfiguration {
+        enableAssetWatcher(commonAssetsWatcher, AssetStore.commonAssetConfig)
+        enableAssetWatcher(worldAssetsWatcher, AssetStore.currentWorldAssetConfig)
+        enableAssetWatcher(levelAssetsWatcher, AssetStore.currentLevelAssetConfig)
+        enableAssetWatcher(specialAssetsWatcher, AssetStore.specialAssetConfig)
+        return this
+    }
+
+    private suspend fun enableAssetWatcher(assetsWatcher: AssetsWatcherConfiguration, assetConfig: AssetModel) {
+        if (!assetsWatcher.enabled && assetsWatcher.toBeStarted) {
+            assetsWatcher.enabled = true
+            assetsWatcher.toBeStarted = false
+
+            resourcesVfs[assetConfig.folderName].apply {
+                if (stat().isDirectory) {
+                    println("Add watcher for '${path}'")
+                    watch {
+                        if (it.kind == Vfs.FileEvent.Kind.MODIFIED) {
+                            checkAssetFolders(it.file, assetsWatcher, assetConfig, coroutineContext)
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private suspend fun checkAssetFolders(file: VfsFile, assetsWatcherCfg: AssetsWatcherConfiguration, assetConfig: AssetModel, assetReloadContext: CoroutineContext) {
 
         // TODO: Currently only fonts, sprite images and parallax images are reloaded
         //       -> Implement reloading also for other asset types
@@ -61,58 +116,13 @@ class AssetReload(private val assetStore: AssetStore = AssetStore) {
                 launchImmediately(context = assetReloadContext) {
                     delay(500)
                     val assetName = config.key
-                    assetStore.fonts[assetName] = Pair(type, resourcesVfs[assetConfig.folderName + "/" + config.value].readBitmapFont(atlas = null))
+                    AssetStore.fonts[assetName] = Pair(assetsWatcherCfg.type, resourcesVfs[assetConfig.folderName + "/" + config.value].readBitmapFont(atlas = null))
                 }
 
                 delay(100)
                 reloading = false
                 println("Finished")
-
-                callback.invoke()
-            }
-        }
-
-        assetConfig.backgrounds.forEach { config ->
-            if (file.fullName.contains(config.value.aseName) && !reloading) {
-                reloading = true  // save that reloading is in progress
-                println("Reloading ${file.fullName}... ")
-
-                launchImmediately(context = assetReloadContext) {
-                    // Give aseprite more time to finish writing the files
-                    delay(100)
-                    val assetName = config.key
-                    assetStore.backgrounds[assetName] = Pair(type, resourcesVfs[assetConfig.folderName + "/" + config.value.aseName].readParallaxDataContainer(config.value, ASE, atlas = null))
-
-                    println("\nTriggering asset change for: $assetName")
-                    world.family { all(Drawable) }.forEach { entity ->
-                        if (entity has Parallax && entity[Parallax].config.name == assetName) {
-                            println("Updating sprite data in entity: ${entity.id}")
-                            onDrawableFamilyRemoved(world, entity)
-                            onDrawableFamilyAdded(world, entity)
-
-                            // We need to update the layer config for the parallax entity - create AnimationScript entity and execute a config function for the parallax entity
-                            world.entity {
-                                it += AnimationScript(
-                                    tweens = listOf(ExecuteConfigFunction(entity = entity, config = entity[Parallax].config, function = configureParallaxLayers))
-                                )
-                            }
-                        }
-                    }
-                    world.family { all(SpecificLayer) }.forEach { entity ->
-                        if (entity[SpecificLayer].parentEntity has Parallax && entity[SpecificLayer].parentEntity[Parallax].config.name == assetName) {
-                            println("Updating layer in entity: ${entity.id} - layer: ${entity[SpecificLayer].spriteLayer}")
-                            onSpecificLayerFamilyRemoved(world, entity)
-                            entity.getOrNull(PositionShape)?.let { it.initialized = false }  // reset position otherwise position data will not be initialized with updated view data (x, y)
-                            onSpecificLayerFamilyAdded(world, entity)
-                        }
-                    }
-                    // Guard period until reloading is activated again - this is used for debouncing watch messages
-                    delay(100)
-                    reloading = false
-                    println("Finished")
-
-                    callback.invoke()
-                }
+                assetsWatcherCfg.fontChangedCallback.forEach { it.invoke() }
             }
         }
         assetConfig.images.forEach { config ->
@@ -121,9 +131,9 @@ class AssetReload(private val assetStore: AssetStore = AssetStore) {
                 println("Reloading ${file.fullName}... ")
 
                 launchImmediately(context = assetReloadContext) {
-                    delay(100)
+                    delay(500)
                     val assetName = config.key
-                    assetStore.images[assetName] = Pair(type,
+                    AssetStore.images[assetName] = Pair(assetsWatcherCfg.type,
                         if (config.value.layers == null) {
                             resourcesVfs[assetConfig.folderName + "/" + config.value.fileName].readImageDataContainer(ASE.toProps(), atlas = null)
                         } else {
@@ -133,30 +143,75 @@ class AssetReload(private val assetStore: AssetStore = AssetStore) {
                         }
                     )
 
-                    println("\nTriggering asset change for: $assetName")
-                    world.family { all(Drawable) }.forEach { entity ->
-                        if (entity has Sprite && entity[Sprite].assetName == assetName) {
-                            println("Updating sprite data in entity: ${entity.id}")
-                            onDrawableFamilyRemoved(world, entity)
-                            onDrawableFamilyAdded(world, entity)
-                        }
-                    }
-                    world.family { all(SpecificLayer) }.forEach { entity ->
-                        if (entity[SpecificLayer].parentEntity has Sprite && entity[SpecificLayer].parentEntity[Sprite].assetName == assetName) {
-                            println("Updating layer in entity: ${entity.id} - layer: ${entity[SpecificLayer].spriteLayer}")
-                            onSpecificLayerFamilyRemoved(world, entity)
-                            entity.getOrNull(PositionShape)?.let { it.initialized = false }  // reset position otherwise position data will not be initialized with updated view data (x, y)
-                            onSpecificLayerFamilyAdded(world, entity)
-                        }
-                    }
-
                     delay(100)
                     reloading = false
                     println("Finished")
-
-                    callback.invoke()
+                    assetsWatcherCfg.imageChangedCallback.forEach { it.invoke() }
                 }
             }
         }
+        assetConfig.backgrounds.forEach { config ->
+            if (file.fullName.contains(config.value.aseName) && !reloading) {
+                reloading = true  // save that reloading is in progress
+                println("Reloading ${file.fullName}... ")
+
+                launchImmediately(context = assetReloadContext) {
+                    // Give aseprite more time to finish writing the files
+                    delay(100)
+                    val assetName = config.key
+                    AssetStore.backgrounds[assetName] = Pair(assetConfig.type, resourcesVfs[assetConfig.folderName + "/" + config.value.aseName].readParallaxDataContainer(config.value, ASE, atlas = null))
+
+                    println("\nTriggering asset change for: $assetName")
+                    // Guard period until reloading is activated again - this is used for debouncing watch messages
+                    delay(100)
+                    reloading = false
+                    println("Finished")
+                    assetsWatcherCfg.backgroundCallback.forEach { it.invoke() }
+                }
+            }
+        }
+    }
+
+    @ThreadLocal
+    companion object {
+        @PublishedApi
+        internal var CURRENT_WATCHER: ResourceDirWatcherConfiguration? = null
+    }
+}
+
+/**
+ * Creates a Watcher for a directory under resources with the given [cfg][ResourceDirWatcherConfiguration].
+ *
+ * @param cfg the [configuration][ResourceDirWatcherConfiguration] of the [ResourceDirWatcher][ResourceDirWatcherConfiguration]
+ * which contains callbacks for each type of assets to be reloaded when they change in Resources directory.
+ */
+suspend fun configureResourceDirWatcher(cfg: ResourceDirWatcherConfiguration.() -> Unit) {
+
+    if (ResourceDirWatcherConfiguration.CURRENT_WATCHER == null) {
+        val resourceDirWatcher = ResourceDirWatcherConfiguration().apply(cfg).configure()
+        ResourceDirWatcherConfiguration.CURRENT_WATCHER = resourceDirWatcher
+    } else {
+        ResourceDirWatcherConfiguration.CURRENT_WATCHER!!.apply(cfg).configure()
+    }
+}
+
+/**
+ * Configure callbacks for asset reloading in game objects.
+ *
+ * @param cfg the [configuration][AssetsWatcherConfiguration] for specifying the callbacks which
+ * will update the assets in specific game objects.
+ */
+fun configureAssetReloading(type: AssetType, cfg: AssetsWatcherConfiguration.() -> Unit) {
+    val currentWatcher = ResourceDirWatcherConfiguration.CURRENT_WATCHER
+    if (currentWatcher != null) {
+        when(type) {
+            AssetType.None -> {}
+            AssetType.Common -> currentWatcher.commonAssetsWatcher.apply(cfg)
+            AssetType.World -> currentWatcher.worldAssetsWatcher.apply(cfg)
+            AssetType.Level -> currentWatcher.levelAssetsWatcher.apply(cfg)
+            AssetType.Special -> currentWatcher.specialAssetsWatcher.apply(cfg)
+        }
+    } else {
+        println("INFO: Asset reloading for type '$type' not applied, because ResourceDirWatcher is not set up!")
     }
 }
