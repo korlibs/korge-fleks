31999e0489d20063e5a85ae0c6b6057a49deef7d
Marko Koschak <marko.koschak@tisno.de>
2024-06-03 18:28:48 +0200

    Update LogoEntityConfig

--- korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/entity/config/LogoEntityConfig.kt
+++ korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/entity/config/LogoEntityConfig.kt
@@ -7,38 +7,44 @@ import korlibs.korge.fleks.components.*
 import korlibs.korge.fleks.entity.*
 import korlibs.korge.fleks.tags.*
 
+
+/**
+ * Config for creation of a logo entity which can consist of multiple layers.
+ * Each layer can be controlled independently e.g. to fade-in or move-in parts
+ * of the logo more dynamically.
+ *
+ * Logo can be centered on the screen and additionally an offset can be specified.
+ */
 data class LogoEntityConfig(
     override val name: String,
 
-    private val x: Float = 0f,
-    private val y: Float = 0f,
     private val assetName: String,
     private val centerX: Boolean = false,
     private val centerY: Boolean = false,
+    private val offsetX: Float = 0f,
+    private val offsetY: Float = 0f,
     private val tint: RgbaComponent.Rgb = RgbaComponent.Rgb.WHITE,
     private val alpha: Float = 1f,
     private val layerIndex: Int,
     private val layerTag: RenderLayerTag
+
 ) : EntityFactory.EntityConfig {
+
     override val configureEntity = fun(world: World, entity: Entity) : Entity = with(world) {
-
-        val globalX: Float? = if (centerX) (AppConfig.TARGET_VIRTUAL_WIDTH - AssetStore.getImageData(assetName).width).toFloat() * 0.5f else null
-        val globalY: Float? = if (centerY) (AppConfig.TARGET_VIRTUAL_HEIGHT - AssetStore.getImageData(assetName).height).toFloat() * 0.5f else null
-
         entity.configure {
             it += PositionComponent(
-                x = globalX ?: this@LogoEntityConfig.x,
-                y = globalY ?: this@LogoEntityConfig.y
+                x = offsetX + (if (centerX) (AppConfig.TARGET_VIRTUAL_WIDTH - AssetStore.getImageData(assetName).width).toFloat() * 0.5f else 0f),
+                y = offsetY + (if (centerY) (AppConfig.TARGET_VIRTUAL_HEIGHT - AssetStore.getImageData(assetName).height).toFloat() * 0.5f else 0f)
             )
             it += SpriteComponent(
-                name = this@LogoEntityConfig.assetName
+                name = assetName
             )
             it += RgbaComponent().apply {
                 tint = this@LogoEntityConfig.tint
                 alpha = this@LogoEntityConfig.alpha
             }
             it += LayerComponent(layerIndex = this@LogoEntityConfig.layerIndex)
-            it += this@LogoEntityConfig.layerTag
+            it += layerTag
 
             // TODO handle logo layers
         }
