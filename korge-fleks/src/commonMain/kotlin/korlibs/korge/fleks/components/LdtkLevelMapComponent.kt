9b5cc053be01ee5e0ec46639947797766d21a6e3
jobeGameDev <marko.koschak@tisno.de>
2024-10-22 17:14:02 +0200

    Store static data of level in asset store (#31)

--- korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/components/LdtkLevelMapComponent.kt
+++ korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/components/LdtkLevelMapComponent.kt
@@ -17,34 +17,15 @@ data class LdtkLevelMapComponent(
      *
      * Example: ["Background", "Playfield"]
      */
-    var layerNames: List<String> = listOf(),
-
-    var levelLayer: String = "",  // The level and layer name in the LDtk world
-
-    // TODO: Move this to AssetStore
-    var width: Float = 0f,  // Size of the level map
-    var height: Float = 0f,
-
-    // internal
-    var initialized: Boolean = false
+    var layerNames: List<String> = listOf()
 ) : CloneableComponent<LdtkLevelMapComponent>() {
     override fun type(): ComponentType<LdtkLevelMapComponent> = LdtkLevelMapComponent
-
-    // Get size of level map and save it into properties of this component
-    override fun World.onAdd(entity: Entity) {
-        // Make sure that initialization is skipped on world snapshot loading (deserialization of save game)
-        if (initialized) return
-        else initialized = true
-
-        val assetStore: AssetStore = this.inject(name = "AssetStore")
-        val tileMapData = assetStore.getTileMapData(levelName, layerNames.first())
-        // TODO: remove hardcoded values - assetStore.getLevelWidth(levelName) : Float
-        width = (tileMapData.data.width * 16).toFloat()
-        height = (tileMapData.data.height * 16).toFloat()
-    }
-
     companion object : ComponentType<LdtkLevelMapComponent>()
 
     // Author's hint: Check if deep copy is needed on any change in the component!
-    override fun clone(): LdtkLevelMapComponent = this.copy()
+    override fun clone(): LdtkLevelMapComponent =
+        LdtkLevelMapComponent(
+            levelName = levelName,
+            layerNames = layerNames.clone()
+        )
 }
