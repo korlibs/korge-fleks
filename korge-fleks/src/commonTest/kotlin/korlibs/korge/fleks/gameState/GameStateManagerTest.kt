987bc9dfad2c7bf003d79781d719bab2972b8f26
Marko Koschak <marko.koschak@tisno.de>
2025-03-12 13:55:59 +0100

    Fix unit tests

--- korge-fleks/src/commonTest/kotlin/korlibs/korge/fleks/gameState/GameStateManagerTest.kt
+++ korge-fleks/src/commonTest/kotlin/korlibs/korge/fleks/gameState/GameStateManagerTest.kt
@@ -2,15 +2,17 @@ package korlibs.korge.fleks.gameState
 
 import com.github.quillraven.fleks.*
 import korlibs.korge.fleks.assets.*
+import korlibs.korge.fleks.gameState.GameStateManager.startGame
 import kotlinx.serialization.modules.SerializersModule
 import kotlin.test.*
 
 class GameStateManagerTest {
 
+    private val gameStateConfig = GameStateConfig("TestGame", 0, true, "world_1", "level_1", "special", "startScript")
+
     @BeforeTest
     fun setup() {
         GameStateManager.assetStore = AssetStore()
-        GameStateManager.gameStateConfig = GameStateConfig("TestGame", true, "world_1", "level_1", "special")
     }
 
     @Test
@@ -23,14 +25,14 @@ class GameStateManagerTest {
     @Test
     fun startGame_startsGameWithValidScript() {
         val world = configureWorld {}
-        GameStateManager.startGame(world)
+        GameStateManager.run { world.startGame(gameStateConfig, loadSnapshot = false) }
         // Verify the game starts correctly (this would require access to internal state or behavior)
     }
 
     @Test
     fun startGame_handlesMissingStartScript() {
         val world = configureWorld {}
-        GameStateManager.startGame(world)
+        GameStateManager.run { world.startGame(gameStateConfig, loadSnapshot = false) }
         // Verify appropriate error handling
     }
 }
