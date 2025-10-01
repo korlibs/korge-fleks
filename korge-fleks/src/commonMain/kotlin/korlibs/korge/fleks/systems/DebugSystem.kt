b3256a7e92335a383bcee219235d58af9ad9cc41
Marko Koschak <marko.koschak@tisno.de>
2025-10-01 08:55:56 +0200

    Add switching camera to debug drone

--- korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/systems/DebugSystem.kt
+++ korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/systems/DebugSystem.kt
@@ -4,6 +4,7 @@ import com.github.quillraven.fleks.*
 import com.github.quillraven.fleks.World.Companion.family
 import korlibs.korge.fleks.components.Debug.Companion.DebugComponent
 import korlibs.korge.fleks.components.Position.Companion.PositionComponent
+import korlibs.korge.fleks.tags.CameraFollowTag
 import korlibs.korge.fleks.utils.*
 import korlibs.math.geom.*
 
@@ -19,6 +20,9 @@ class DebugSystem: IteratingSystem(
     private var xPos = 0f
     private var yPos = 0f
 
+    private val cameraFollowFamily = family { all(CameraFollowTag) }
+    private var playerEntity: Entity = Entity.NONE
+
     /**
      * This function is used to move the debug entity to the given position. This is useful for quickly
      * moving inside the game world to a specific position for debugging purposes.
@@ -33,7 +37,6 @@ class DebugSystem: IteratingSystem(
     override fun onTickEntity(entity: Entity) {
         val camera: Entity = world.getMainCamera()
 
-
         if (positionTrigger) {
             positionTrigger = false
 
@@ -43,7 +46,22 @@ class DebugSystem: IteratingSystem(
             positionComponent.y = yPos
             positionComponent.run { world.convertToWorldCoordinates(camera) }
 
+            // Attach camera to debug entity and remove from player entity
+            if (entity hasNo CameraFollowTag) {
+                cameraFollowFamily.forEach {
+                    playerEntity = it
+                    it.configure { it -= CameraFollowTag }
+                }
+                entity.configure { it += CameraFollowTag }
+            }
+        } else {
+            // Attach camera to player entity again
+            if (entity has CameraFollowTag) {
+                entity.configure { it -= CameraFollowTag }
+                playerEntity.configure { it += CameraFollowTag }
+            }
         }
+
 
     }
 }
