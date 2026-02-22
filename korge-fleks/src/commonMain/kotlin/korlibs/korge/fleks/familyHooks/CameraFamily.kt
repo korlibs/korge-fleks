6399d57c5d067c1938dd3e86a7c453cd217d629f
Marko Koschak <marko.koschak@tisno.de>
2026-02-22 21:37:57 +0100

    Remove unsave family creation for CameraFamily

--- korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/familyHooks/CameraFamily.kt
+++ korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/familyHooks/CameraFamily.kt
@@ -1,15 +1,11 @@
 package korlibs.korge.fleks.familyHooks
 
 import com.github.quillraven.fleks.*
-import korlibs.korge.fleks.components.Position.Companion.PositionComponent
 import korlibs.korge.fleks.systems.SystemRuntimeConfigs
-import korlibs.korge.fleks.tags.*
 
 /**
  *
  */
-val mainCameraFamily = World.family { all(MainCameraTag, PositionComponent) }
-
 val onMainCameraAdded: FamilyHook = { entity ->
     val systemRuntimeConfigs = inject<SystemRuntimeConfigs>("SystemRuntimeConfigs")
     systemRuntimeConfigs.camera = entity
