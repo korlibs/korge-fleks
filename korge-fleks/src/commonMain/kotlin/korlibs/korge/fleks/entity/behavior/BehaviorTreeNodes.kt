834097f8d944699081627860736810298496c05d
Marko Koschak <marko.koschak@tisno.de>
2026-05-10 12:48:48 +0200

    Refactor SelectorNode and SequenceNode to use vararg for children

--- korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/entity/behavior/BehaviorTreeNodes.kt
+++ korge-fleks/src/commonMain/kotlin/korlibs/korge/fleks/entity/behavior/BehaviorTreeNodes.kt
@@ -27,10 +27,14 @@ class EmptyTreeNode : BTNode {
  * or perform some action when the selector fails, which can be helpful for debugging or for triggering certain
  * behaviors in the game when a certain branch of the behavior tree fails.
  */
-class SelectorNode(
-    private val children: List<BTNode>,
-    private val failureCall: () -> Unit = {}
-) : BTNode {
+class SelectorNode private constructor() : BTNode {
+    private lateinit var children: List<BTNode>
+
+    constructor(vararg children: BTNode) : this() {
+        if (children.isEmpty()) throw IllegalArgumentException("SelectorNode must have at least one child")
+        // Create a list of children from the vararg parameter. We use a vararg parameter for convenience when constructing the behavior tree.
+        this.children = children.toList()
+    }
 
     override fun World.tick(entity: Entity, deltaTime: Float): BTStatus {
         children.forEach { child ->
@@ -40,7 +44,7 @@ class SelectorNode(
                 BTStatus.Failure -> {}
             }
         }
-        failureCall.invoke()
+        //println("WARNING: SelectorNode '${this::class.simpleName}' failed all children")
 
         return BTStatus.Failure
     }
@@ -51,7 +55,15 @@ class SelectorNode(
  * Second Composite Node: A sequence node (symbol: [->]) that ticks its children in order and returns failure on the
  * first child that fails, success if all children succeed, and running if any child is still running.
  */
-class SequenceNode(private val children: List<BTNode>) : BTNode {
+class SequenceNode private constructor() : BTNode {
+    private lateinit var children: List<BTNode>
+
+    constructor(vararg children: BTNode) : this() {
+        if (children.isEmpty()) throw IllegalArgumentException("SequenceNode must have at least one child")
+        // Create a list of children from the vararg parameter. We use a vararg parameter for convenience when constructing the behavior tree.
+        this.children = children.toList()
+    }
+
     override fun World.tick(entity: Entity, deltaTime: Float): BTStatus {
         children.forEach { child ->
             when (child.run { tick(entity, deltaTime) }) {
@@ -62,6 +74,7 @@ class SequenceNode(private val children: List<BTNode>) : BTNode {
         }
         return BTStatus.Success
     }
+
 }
 
 /**
